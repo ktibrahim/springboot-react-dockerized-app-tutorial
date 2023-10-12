70d9336fae6b04a625892b4d6f9d13d43774c963
Talal Ibrahim <ibrahim@ebi.ac.uk>
2023-10-12 17:58:34 +0100

    add request intercept filter for react app

---
+++ src/main/java/com/ontology/tools/developertest/config/ReactRequestForwardFilter.java
@@ -0,0 +1,25 @@
+package com.ontology.tools.developertest.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Configuration
+public class ReactRequestForwardFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+        if (!path.startsWith("/api") && !path.contains(".") && path.matches("/(.*)")) {
+            request.getRequestDispatcher("/").forward(request, response);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
